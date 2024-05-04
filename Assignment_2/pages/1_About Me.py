98333f9ecc04cc116669a46f85c89c1ef8a896b8
Fibgreen <167343829+Fibgreen@users.noreply.github.com>
2024-05-04 00:00:36 -0400

    Add files via upload

---
+++ Assignment_2/pages/1_About Me.py
@@ -0,0 +1,31 @@
+## Notes
+# Almost done, make it look pretty
+# Add some images
+
+import streamlit as st
+
+st.title('About Me')
+st.divider()
+
+st.header('Name')
+st.write('Connor S. Green')
+
+st.header('Birthday')
+st.write("March 9th, 2000")
+
+st.header('Hometown')
+st.write('Norfolk, VA')
+
+st.header('Hobbies')
+st.write('Sailing')
+st.image('./images/sailing.JPG')
+st.write('Racing')
+st.image('./images/racing.png')
+
+st.header('Career Goals')
+st.write('After graduation I will commission into the Navy and go '
+         'to flight school')
+
+st.header('ME!')
+st.image('./images/me.png')
+
